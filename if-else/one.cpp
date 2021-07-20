9a3fbe47447d65edcba1655067e72dfff964fce5
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-07-20 22:18:47 +0000

    added if else

---
+++ if-else/one.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int year = 2022;
+    string name = (year ==2021)?"Mariem":"unknow";
+    if(name =="Mariem"){
+        cout << "She is the best a girl than in the world "<<
+        endl<< "I love her so much";
+    }else{
+        cout << "I don't have any idea about her😂";
+    }
+    return 0;
+}
