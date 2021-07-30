881c3c1ea260a100ddf971de085818b22bf0aef2
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-07-30 18:48:23 +0000

    for loop with  array

--- arrays/index.cpp
+++ arrays/index.cpp
@@ -11,5 +11,10 @@ int main(){
     int arr2[5] = {0};
     cout << arr2[2] << endl;
 
-    cout << sizeof(arr2) / sizeof(arr2[0]);
+    cout << sizeof(arr2) / sizeof(arr2[0]) << endl;
+
+    int arr3 = sizeof(arr) / sizeof(arr[0]);
+    for(int i=0; i<arr3; i++){
+        cout << arr[i] << endl;
+    }
 }
