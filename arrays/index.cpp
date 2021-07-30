b7a4dab56fead8f0eb455180ab3a08c3a818a3e0
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-07-30 18:29:42 +0000

    added array

--- arrays/index.cpp
+++ arrays/index.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 int main(){
     string arr[3] = {"sheikh","moctar","Ahmed"};
-    cout << arr[0];
+    cout << arr[2] << endl;
+    
+    arr[2] = "Jidou";
+    cout << arr[2];
 }
