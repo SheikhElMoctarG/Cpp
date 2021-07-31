7e56f86eb52d3c8d7400871be4dee645cb5b9146
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-07-31 17:36:31 +0000

    added argument defualt

--- functions/index.cpp
+++ functions/index.cpp
@@ -4,14 +4,14 @@ using namespace std;
 void seting(string name) {
     cout << "Hi "<<name;
 }
-int multi(int n, int n2){
+int multi(int n=1, int n2=2){
     return n * n2;
 }
 int main(){
     seting("Sheikh");
     cout << endl<< endl;
     
-    cout << multi(2,8);
+    cout << multi();
 
     return 0;
 }
