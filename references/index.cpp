22cb179161c2293cb2763fd4b0d2f8c2ba74e680
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-08-01 13:05:21 +0000

    added references for variables

---
+++ references/index.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 3;
+    string s = "Sheikh";
+
+    cout << "references for variable s: "<<&s<<endl;
+    cout << "references for variable a: "<<&a<<endl;
+
+    return 0;
+}
