4031aef533e466065f3a3c3fe66965b77b4702b8
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-08-02 15:12:54 +0000

    upgrade pointers

--- pointers/index.cpp
+++ pointers/index.cpp
@@ -5,6 +5,15 @@ int main(){
     string* l = &lang;
 
     cout << "My lang pointer = "<<l<<endl;
-    cout << "value of a pointer = "<<*l;
+    cout << "value of a pointer = "<<*l<<endl;
+
+    int x = 1;
+    int y = 2;
+    int* ptr;
+    ptr = &x;
+    cout << "*ptr = " << *ptr << endl;
+    ptr = &y;
+    cout << "*ptr = " << *ptr << endl;
+
     return 0;
 }
