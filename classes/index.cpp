46f946b07c73b46c449d0a114d1db953dc1d0a1e
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-08-06 20:31:19 +0000

    Addedd classes

---
+++ classes/index.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    public:
+      string name;
+      int age;
+      int salary;
+};
+
+int main(){
+    Employee Sheikh;
+    Employee Jidou;
+    Sheikh.name = "Sheikh El-Moctar";
+    Sheikh.age = 17;
+    Sheikh.salary = 3000;
+
+    Jidou.name = "Jidou Muhammed lemine";
+    Jidou.age = 18;
+    Jidou.salary = 1500;
+
+    cout << Sheikh.name <<endl;
+    cout << Jidou.name<<endl;
+}
+
